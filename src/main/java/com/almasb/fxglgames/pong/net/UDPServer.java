0e68687394c55cfc947ccd0c2598b9b5ae55c276
Alex Dauncey <84759985+Shylexx@users.noreply.github.com>
2022-11-15 17:07:57 +0000

    UDP Echoing, sending to clients

--- src/main/java/com/almasb/fxglgames/pong/net/UDPServer.java
+++ src/main/java/com/almasb/fxglgames/pong/net/UDPServer.java
@@ -5,6 +5,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class UDPServer extends Thread{
@@ -35,12 +36,25 @@ public class UDPServer extends Thread{
 
             InetAddress address = packet.getAddress();
             int port = packet.getPort();
-            packet = new DatagramPacket(buf, buf.length, address, port);
+
             String received = new String(packet.getData(), packet.getOffset(), packet.getLength(), StandardCharsets.UTF_8);
 
             System.out.println(received);
+            System.out.println("Received from IP: " + address.toString() + " and port: " + port);
             if (received.equals("end")) {
                 running = false;
+            }
+            String toSend = "Echo " + received;
+            byte[] byteAry = new byte[512];
+            System.arraycopy(toSend.getBytes(StandardCharsets.UTF_8), 0, byteAry, 0, toSend.length());
+            System.out.println(byteAry.toString());
+            // Send echo
+            //packet = new DatagramPacket(byteAry, byteAry.length, address, port);
+            packet = new DatagramPacket(byteAry, byteAry.length, address, port);
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
             }
         }
         socket.close();
